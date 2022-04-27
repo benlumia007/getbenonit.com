a2018ae52ba5778bceba0c77c27a9e2da4bdef50
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-27 10:35:43 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Default index template
+ * 
+ * This file is used to bootstrap the theme
+ * 
+ * @package   getbenonit.com
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://getbenonit.com
+ */
+
+/**
+ * Autoload
+ * 
+ * Auto-load classes and files via the Composer autoloader.
+ */
+if ( file_exists( 'vendor/autoload.php' ) ) {
+    require_once( 'vendor/autoload.php' );
+}
